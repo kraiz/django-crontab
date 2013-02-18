d62d3bc1a77457474ea7f2e39d334e1d76d18252
Lars Kreisz <der.kraiz@gmail.com>
2013-02-18 17:58:08 +0100

    version bump with changelog entry (file extension path fix)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='django-crontab',
     description='dead simple crontab powered job scheduling for django',
-    version='0.4.2',
+    version='0.4.3',
     author='Lars Kreisz',
     author_email='der.kraiz@gmail.com',
     license='MIT',
