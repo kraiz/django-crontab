0ec1b0a3deef298bb79d9c245569821b04e683d4
arski <martin@webscio.net>
2014-10-07 21:31:03 +0100

    Move Travis tests out of Django

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
 import pkgutil
 import unittest
 
+
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     module = loader.find_module(module_name).load_module(module_name)
     for name in dir(module):
