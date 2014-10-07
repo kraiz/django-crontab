d227ff3a50be92bbf6c7049f9ab50c8f958fa0ca
arski <martin@webscio.net>
2014-10-07 16:49:50 +0100

    Fix import for python 2.6 compat

--- django_crontab/tests/__init__.py
+++ django_crontab/tests/__init__.py
@@ -5,5 +5,5 @@ for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     module = loader.find_module(module_name).load_module(module_name)
     for name in dir(module):
         obj = getattr(module, name)
-        if isinstance(obj, type) and issubclass(obj, unittest.case.TestCase):
+        if isinstance(obj, type) and issubclass(obj, unittest.TestCase):
             exec ('%s = obj' % obj.__name__)
