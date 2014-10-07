f211fe6111e82f0877ad3474de008901b176d96a
arski <martin@webscio.net>
2014-10-07 16:30:04 +0100

    Create a dynamic tests importer inside __init__

--- django_crontab/tests/__init__.py
+++ django_crontab/tests/__init__.py
@@ -1,1 +1,9 @@
-from test_crontab import CrontabTestCase
+import pkgutil
+import unittest
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    module = loader.find_module(module_name).load_module(module_name)
+    for name in dir(module):
+        obj = getattr(module, name)
+        if isinstance(obj, type) and issubclass(obj, unittest.case.TestCase):
+            exec ('%s = obj' % obj.__name__)
