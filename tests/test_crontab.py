a5a31e96cba127b21afba304df3b13c0c748ff2a
arski <martin@webscio.net>
2014-10-08 09:58:11 +0100

    Fix Travis again, thanks

--- tests/test_crontab.py
+++ tests/test_crontab.py
@@ -32,5 +32,5 @@ def test_add_jobs():
     crontab = Crontab()
     crontab.add_jobs()
 
-    expected_crontab = ['*/5 * * * *  ' + sys.executable + ' ' + settings.CRONTAB_DJANGO_MANAGE_PATH + ' crontab run eb868be6b69c31faa6b03a4cf0dd3d8c   # django-cronjobs for fail\n']
+    expected_crontab = ['*/5 * * * *  ' + sys.executable + ' ' + settings.CRONTAB_DJANGO_MANAGE_PATH + ' crontab run eb868be6b69c31faa6b03a4cf0dd3d8c   # django-cronjobs for tests\n']
     assert_equal(expected_crontab, crontab.crontab_lines)
