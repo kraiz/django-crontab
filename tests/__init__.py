e16e3057b0e7d7cff2ddf42b5c3568753409427a
arski <martin@webscio.net>
2014-10-08 09:45:45 +0100

    Remove deprecated setup_environ

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,7 @@
-from django.core.management import setup_environ
+import os
+
 from django.test.utils import setup_test_environment
 
-import settings
 
-
-setup_environ(settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 setup_test_environment()
