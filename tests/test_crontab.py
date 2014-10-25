4cef6bc624c75593b3b13f5834e5c90017e03e68
kraiz <lars.kreisz@gmail.com>
2014-10-25 14:37:32 +0200

    added testcase adding many different cronjobs

--- tests/test_crontab.py
+++ tests/test_crontab.py
@@ -30,6 +30,7 @@ def test_read_crontab(mock_popen):
 
     mock_popen.assert_called_with('/usr/bin/crontab -l')
 
+
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job')])
 @patch('os.popen')
 @patch('os.system')
@@ -55,12 +56,38 @@ def test_read_write_crontab(mock_system, mock_popen):
 
 
 @override_settings(CRONJOBS=[('*/5 * * * *', 'myproject.myapp.cron.my_scheduled_job')])
-def test_add_jobs():
+def test_add_single_simple_job():
     crontab = Crontab()
     crontab.add_jobs()
+    expected_line = ['*/5 * * * *  %(exe)s %(manage)s crontab run eb868be6b69c31faa6b03a4cf0dd3d8c   # django-cronjobs for tests\n' % {
+        'exe': sys.executable,
+        'manage': settings.CRONTAB_DJANGO_MANAGE_PATH
+    }]
+    assert_equal(expected_line, crontab.crontab_lines)
 
-    expected_crontab = ['*/5 * * * *  ' + sys.executable + ' ' + settings.CRONTAB_DJANGO_MANAGE_PATH + ' crontab run eb868be6b69c31faa6b03a4cf0dd3d8c   # django-cronjobs for tests\n']
-    assert_equal(expected_crontab, crontab.crontab_lines)
+
+@override_settings(CRONJOBS=[
+    ('*/5 * * * *', 'myproject.myapp.cron.my_scheduled_job'),
+    ('* 3 * * *', 'tests.cron.cron_job', '> /home/myhome/logs/cron_job.log'),
+    ('0 4 * * *', 'tests.cron.cron_job', [1, 'two'], {'test': 34, 'a': 's2'}),
+    ('1 2 */5 * 0', 'tests.cron.cron_job', [1, 'two'], {'test': 34, 'a': 's2'}, 'suffix'),
+    ('@reboot', 'tests.cron.cron_job', [], {}, 'suffix'),
+])
+def test_add_many_different_jobs():
+    crontab = Crontab()
+    crontab.add_jobs()
+    ctx = dict(exe=sys.executable, manage=settings.CRONTAB_DJANGO_MANAGE_PATH)
+    expected_lines = [
+        '*/5 * * * *  %(exe)s %(manage)s crontab run eb868be6b69c31faa6b03a4cf0dd3d8c   # django-cronjobs for tests\n' % ctx,
+        '* 3 * * *  %(exe)s %(manage)s crontab run c03a5151588fde26da760240ed6a9b9a > /home/myhome/logs/cron_job.log  # django-cronjobs for tests\n' % ctx,
+        '0 4 * * *  %(exe)s %(manage)s crontab run e5b6fc0d28edb93283faf23e808b1065   # django-cronjobs for tests\n' % ctx,
+        '1 2 */5 * 0  %(exe)s %(manage)s crontab run e1d364332622aa2c382fcb325a5b388a suffix  # django-cronjobs for tests\n' % ctx,
+        '@reboot  %(exe)s %(manage)s crontab run ed4d4f3082654defa6f5acd75aaead34 suffix  # django-cronjobs for tests\n' % ctx,
+    ]
+    assert_equal(len(expected_lines), len(crontab.crontab_lines))
+    for expected, actual in zip(expected_lines, crontab.crontab_lines):
+        assert_equal(expected, actual)
+
 
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job')])
 @patch('tests.cron.cron_job')
@@ -69,12 +96,14 @@ def test_run_no_arg_format1_job(method_to_call):
     crontab.run_job('4f30993ab69a8c5763ce55f762ef0433')
     method_to_call.assert_called_with()
 
+
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job', '> /home/myhome/logs/cron_job.log')])
 @patch('tests.cron.cron_job')
 def test_run_no_arg_format1_job_with_suffix(method_to_call):
     crontab = Crontab()
     crontab.run_job('53e6fe5b66bd870e396d574ba1503c33')
     method_to_call.assert_called_with()
+
 
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job', [1, 'two'])])
 @patch('tests.cron.cron_job')
@@ -83,12 +112,14 @@ def test_run_args_only_format2_job(method_to_call):
     crontab.run_job('369f8418b0f8cf1fff78c547516aa8e0')
     method_to_call.assert_called_with(1, 'two')
 
+
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job', [1, 'two'], {'test': 34, 'a': 's2'})])
 @patch('tests.cron.cron_job')
 def test_run_format2_job(method_to_call):
     crontab = Crontab()
     crontab.run_job('13e8169dffe273b8b0c5f8abe1b6f643')
     method_to_call.assert_called_with(1, 'two', test=34, a='s2')
+
 
 @override_settings(CRONJOBS=[('*/1 * * * *', 'tests.cron.cron_job', [1, 'two'], dict(), 'some suffix')])
 @patch('tests.cron.cron_job')
