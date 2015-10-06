572fdc1145806646ba1338b56b258d58699bd3e6
kraiz <lars.kreisz@gmail.com>
2015-10-06 21:01:01 +0200

    make command use argparse (default since django 1.8)

--- django_crontab/management/commands/crontab.py
+++ django_crontab/management/commands/crontab.py
@@ -5,38 +5,27 @@ from django_crontab.crontab import Crontab
 
 
 class Command(BaseCommand):
-    args = '<add|show|remove>'
     help = 'run this command to add, show or remove the jobs defined in CRONJOBS setting from/to crontab'
-    crontab_lines = []
 
     def add_arguments(self, parser):
-        parser.add_argument('subcommand', help='subcommand to run, see help')
-        parser.add_argument('jobhash', nargs='?', default='')
+        parser.add_argument('subcommand', choices=['add', 'show', 'remove', 'run'])
+        parser.add_argument('jobhash', nargs='?')
 
     def handle(self, *args, **options):
         """
         Dispatches by given subcommand
         """
-        if len(args) > 0:
-            if args[0] == 'add':
-                with Crontab(**options) as crontab:
-                    crontab.remove_jobs()
-                    crontab.add_jobs()
-                return
-            elif args[0] == 'show':
-                with Crontab(readonly=True, **options) as crontab:
-                    crontab.show_jobs()
-                return
-            elif args[0] == 'remove':
-                with Crontab(**options) as crontab:
-                    crontab.remove_jobs()
-                return
-            elif args[0] == 'run':
-                if len(args) > 1:
-                    jobhash = args[1]
-                else:
-                    jobhash = options['jobhash']
-
-                Crontab().run_job(jobhash)
-                return
-        print(help)
+        if options['subcommand'] == 'add':
+            with Crontab(**options) as crontab:
+                crontab.remove_jobs()
+                crontab.add_jobs()
+        elif options['subcommand'] == 'show':
+            with Crontab(readonly=True, **options) as crontab:
+                crontab.show_jobs()
+        elif options['subcommand'] == 'remove':
+            with Crontab(**options) as crontab:
+                crontab.remove_jobs()
+        elif options['subcommand'] == 'run':
+            Crontab().run_job(options['jobhash'])
+        else:
+            print(self.help)
