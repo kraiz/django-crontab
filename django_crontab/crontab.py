9cd7e86af31c52545bc685e7b6842b92fdc1aab3
David Jiang <David Jiang>
2017-03-08 17:44:55 -0500

    add comments to the `Crontab` class in `crontab.py` and the `Command` class in `management/commands/crontab.py`

--- django_crontab/crontab.py
+++ django_crontab/crontab.py
@@ -27,6 +27,11 @@ class Crontab(object):
         self.crontab_lines = []
         self.settings = Settings(settings)
 
+    """
+    Instead of interacting with the crontab file directly,
+    Have read and write methods that trigger on enter and exit,
+    so changes within the with block can be committed all at once.
+    """
     def __enter__(self):
         """
         Automatically read crontab when used as with statement
@@ -51,18 +56,23 @@ class Crontab(object):
         """
         Writes internal buffer back to crontab
         """
+        # create a temporary file using mkstemp to avoid race conditions
         fd, path = tempfile.mkstemp()
+        # write all the lines in the internal buffer to the temporary file
         tmp = os.fdopen(fd, 'w')
         for line in self.crontab_lines:
             tmp.write(line)
         tmp.close()
+        # replace the contab with the temporary file
         os.system('%s %s' % (self.settings.CRONTAB_EXECUTABLE, path))
+        # delete the temporary file
         os.unlink(path)
 
     def add_jobs(self):
         """
         Adds all jobs defined in CRONJOBS setting to internal buffer
         """
+        # take all jobs specified in settings
         for job in self.settings.CRONJOBS:
             # differ format and find job's suffix
             if len(job) > 2 and isinstance(job[2], string_type):
@@ -73,6 +83,8 @@ class Crontab(object):
             else:
                 job_suffix = ''
 
+            # format the job into crontab syntax using the format string and relevant variables as specified in settings
+            # and append it to the internal buffer
             self.crontab_lines.append(self.settings.CRONTAB_LINE_PATTERN % {
                 'time': job[0],
                 'comment': self.settings.CRONTAB_COMMENT,
@@ -87,6 +99,7 @@ class Crontab(object):
                     self.settings.COMMAND_SUFFIX
                 ]))
             })
+            # output the action if the verbose option is specified
             if self.verbosity >= 1:
                 print('  adding cronjob: (%s) -> %s' % (self.__hash_job(job), job))
 
@@ -95,9 +108,13 @@ class Crontab(object):
         Print the jobs from from crontab
         """
         print(u'Currently active jobs in crontab:')
+        # iterate through all the lines in the internal buffer
         for line in self.crontab_lines[:]:
+            # check if the line describes a crontab job
             job = self.settings.CRONTAB_LINE_REGEXP.findall(line)
+            # if the job is generated using django_crontab for this application
             if job and job[0][4] == self.settings.CRONTAB_COMMENT:
+                # output the job hash and details if the verbose option is specified
                 if self.verbosity >= 1:
                     print(u'%s -> %s' % (
                         job[0][2].split()[4],
@@ -108,10 +125,15 @@ class Crontab(object):
         """
         Removes all jobs defined in CRONJOBS setting from internal buffer
         """
+        # iterate through all the lines in the internal buffer
         for line in self.crontab_lines[:]:
+            # check if the line describes a crontab job
             job = self.settings.CRONTAB_LINE_REGEXP.findall(line)
+            # if the job is generated using django_crontab for this application
             if job and job[0][4] == self.settings.CRONTAB_COMMENT:
+                # remove the line from the internal buffer
                 self.crontab_lines.remove(line)
+                # output the action if the verbose option is specified
                 if self.verbosity >= 1:
                     print('removing cronjob: (%s) -> %s' % (
                         job[0][2].split()[4],
@@ -123,31 +145,41 @@ class Crontab(object):
         """
         Executes the corresponding function defined in CRONJOBS
         """
+
+        # obtain the job tuple from the hash
         job = self.__get_job_by_hash(job_hash)
         job_name = job[1]
         job_args = job[2] if len(job) > 2 and not isinstance(job[2], string_type) else []
         job_kwargs = job[3] if len(job) > 3 else {}
 
         lock_file_name = None
+        # if the LOCK_JOBS option is specified in settings
         if self.settings.LOCK_JOBS:
+            # create and open a lock file
             lock_file = open(os.path.join(tempfile.gettempdir(), 'django_crontab_%s.lock' % job_hash), 'w')
             lock_file_name = lock_file.name
             try:
+                # acquire the lock
                 fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
             except IOError:
                 logger.warning('Tried to start cron job %s that is already running.', job)
                 return
 
+        # parse the module and function names from the job
         module_path, function_name = job_name.rsplit('.', 1)
+        # import the module and get the function
         module = import_module(module_path)
         func = getattr(module, function_name)
+        # run the function
         try:
             func(*job_args, **job_kwargs)
         except:
             logger.exception('Failed to complete cronjob at %s', job)
 
+        # if the LOCK_JOBS option is specified in settings
         if self.settings.LOCK_JOBS:
             try:
+                # release the lock
                 fcntl.flock(lock_file, fcntl.LOCK_UN)
             except IOError:
                 logger.exception('Error unlocking %s', lock_file_name)
@@ -157,7 +189,9 @@ class Crontab(object):
         """
         Builds an md5 hash representing the job
         """
+        # encode the job into a JSON string
         j = json.JSONEncoder(sort_keys=True).encode(job)
+        # produce a md5 hash from the JSON string
         h = hashlib.md5(j.encode('utf-8')).hexdigest()
         return h
 
@@ -165,7 +199,9 @@ class Crontab(object):
         """
         Finds the job by given hash
         """
+        # iterate through all jobs as specified in settings
         for job in self.settings.CRONJOBS:
+            # return the job if the hash of the job matches the given hash
             if self.__hash_job(job) == job_hash:
                 return job
         raise RuntimeError(
--- django_crontab/management/commands/crontab.py
+++ django_crontab/management/commands/crontab.py
@@ -5,6 +5,11 @@ from django_crontab.crontab import Crontab
 
 
 class Command(BaseCommand):
+    """
+    Define the Django management commands add, show, remove and run
+    run takes an additional `jobhash` argument specifying which job to run
+    """
+
     help = 'run this command to add, show or remove the jobs defined in CRONJOBS setting from/to crontab'
 
     def add_arguments(self, parser):
@@ -15,17 +20,19 @@ class Command(BaseCommand):
         """
         Dispatches by given subcommand
         """
-        if options['subcommand'] == 'add':
-            with Crontab(**options) as crontab:
-                crontab.remove_jobs()
-                crontab.add_jobs()
-        elif options['subcommand'] == 'show':
+        if options['subcommand'] == 'add':        # add command
+            with Crontab(**options) as crontab:   # initialize a Crontab class with any specified options
+                crontab.remove_jobs()             # remove all jobs specified in settings from the crontab
+                crontab.add_jobs()                # and add them back
+        elif options['subcommand'] == 'show':     # show command
+            # initialize a readonly Crontab class with any specified options
             with Crontab(readonly=True, **options) as crontab:
-                crontab.show_jobs()
-        elif options['subcommand'] == 'remove':
-            with Crontab(**options) as crontab:
-                crontab.remove_jobs()
-        elif options['subcommand'] == 'run':
-            Crontab().run_job(options['jobhash'])
+                crontab.show_jobs()               # list all currently active jobs from crontab
+        elif options['subcommand'] == 'remove':   # remove command
+            with Crontab(**options) as crontab:   # initialize a Crontab class with any specified options
+                crontab.remove_jobs()             # remove all jobs specified in settings from the crontab
+        elif options['subcommand'] == 'run':      # run command
+            Crontab().run_job(options['jobhash']) # run the job with the specified hash
         else:
+            # output the help string if the user entered something not specified above
             print(self.help)
