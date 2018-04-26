60c40ca0c5c1aceb51f86237bbb4def4e1a69118
kraiz <lars.kreisz@gmail.com>
2018-04-26 22:43:58 +0200

    seems like nose already calls this

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,14 @@
 import os
 
-from django.test.utils import setup_test_environment, teardown_test_environment
+# from django.test.utils import setup_test_environment, teardown_test_environment
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-try:
-    # Prevent if setup_test_environment has been called previously
-    teardown_test_environment()
-except AttributeError:
-    pass
+# try:
+#     # Prevent if setup_test_environment has been called previously
+#     teardown_test_environment()
+# except AttributeError:
+#     pass
 
-setup_test_environment()
+# setup_test_environment()
