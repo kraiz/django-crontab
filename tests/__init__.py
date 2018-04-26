93224bb732b99367448586391e04438c99adb156
kraiz <lars.kreisz@gmail.com>
2018-04-26 23:05:39 +0200

    works fine without

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,3 @@
 import os
 
-# from django.test.utils import setup_test_environment, teardown_test_environment
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-# try:
-#     # Prevent if setup_test_environment has been called previously
-#     teardown_test_environment()
-# except AttributeError:
-#     pass
-
-# setup_test_environment()
